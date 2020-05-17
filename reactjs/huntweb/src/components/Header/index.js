f98bb539915a1941cfad5a77e599a8ac662f368d
Cristiana Passos <cristiana.passos@bee-eng.pt>
2020-05-17 21:03:04 +0100

    create react project

---
+++ reactjs/huntweb/src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles.css";
+
+//statelles componente criar componentes com apenas functions
+const Header = () => {
+  return <header id="main-header">JSHunt</header>;
+};
+
+export default Header;
