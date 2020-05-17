ddff9e8d472965c1dd5e22dce0f9635b3fafc9ef
Cristiana Passos <cristiana.passos@bee-eng.pt>
2020-05-17 22:11:30 +0100

    load products

--- reactjs/huntweb/src/pages/main/index.js
+++ reactjs/huntweb/src/pages/main/index.js
@@ -1,11 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import api from "../../services/api";
 
 function Main() {
+  const [products, setProducts] = useState([]);
+
   useEffect(() => {
     async function loadProducts() {
       const response = await api.get("/products");
       console.log(response.data.docs);
+      setProducts([...response.data.docs]);
     }
 
     loadProducts();
@@ -13,7 +16,7 @@ function Main() {
 
   return (
     <div>
-      <h1>Hi</h1>
+      {products.map(product => <h2 key={product._id}>{product.title}</h2>)}
     </div>
   );
 }
