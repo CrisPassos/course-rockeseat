3a7d48d6c7b391bc6cdba3f2d462a754d6f81fcc
Cristiana Passos <cristiana.passos@bee-eng.pt>
2020-05-24 21:16:31 +0100

    connect to database

--- nodejs/server.js
+++ nodejs/server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 //start mongoose
-mongoose.connect("mongodb://localhost:27017/nodeapi", {
+mongoose.connect("", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
